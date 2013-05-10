e6f2bd2b617798e54edca313ff0ab0528d74c0f4
Raphael Eidus <raphaeleidus@gmail.com>
2013-05-10 08:00:38 -0400

    more tests

--- test/grunt-lazyload_test.js
+++ test/grunt-lazyload_test.js
@@ -23,10 +23,30 @@ var grunt_lazyload = require('../lib/grunt-lazyload.js');
 */
 
 var grunt, loader, gruntInjecter = function() {
-  var callCounts = {registerTask: 0};
+  var callCounts = {registerTask: 0, run: 0, renameTask: 0, loadNpmTasks: 0};
+  var tasks = [];
   return {
     task: {
-      registerTask: function() { callCounts.registerTask++; }
+      registerTask: function(taskName, description, func) { 
+        callCounts.registerTask++;
+        tasks.push({name:taskName, task: func});
+      },
+      run: function(taskName) {
+        callCounts.run++;
+        var toRun = tasks.filter(function(t) { return t.name === taskName; });
+        toRun.forEach(function(t) { t.task(); });
+      },
+      renameTask: function(orinalName, newName) {
+        callCounts.renameTask++;
+        tasks.forEach(function(t) {
+          if(t.name === orinalName) {
+            t.name = newName;
+          }
+        });
+      }
+    },
+    loadNpmTasks: function() {
+      callCounts.loadNpmTasks++;
     },
     getCurretCallCounts: function() { return callCounts; }
   };
@@ -42,15 +62,39 @@ exports['lazyload'] = {
   'single task register': function(test) {
     test.expect(1);
     // tests here
-    loader('PackageName', 'task');
+    loader('PackageName', 'singleTask');
     test.equal(grunt.getCurretCallCounts().registerTask, 1, 'registerTask should be called once');
     test.done();
   },
-  'array': function(test) {
+  'single task register and run': function(test) {
+    test.expect(4);
+    // tests here
+    loader('PackageName', 'singleTask');
+    grunt.task.run('singleTask');
+    var callCounts = grunt.getCurretCallCounts();
+    test.equal(callCounts.registerTask, 1, 'registerTask should be called once');
+    test.equal(callCounts.run, 2, 'run should be called twice');
+    test.equal(callCounts.renameTask, 1, 'renameTask should be called once');
+    test.equal(callCounts.loadNpmTasks, 1, 'loadNpmTasks should be called once');
+    test.done();
+  },
+  'array task register': function(test) {
     test.expect(1);
     // tests here
     loader('PackageName', ['task1', 'task2', 'task3']);
     test.equal(grunt.getCurretCallCounts().registerTask, 3, 'registerTask should be called three times');
+    test.done();
+  },
+  'array task register and run': function(test) {
+    test.expect(4);
+    // tests here
+    loader('PackageName', ['task1', 'task2', 'task3']);
+    grunt.task.run('task2');
+    var callCounts = grunt.getCurretCallCounts();
+    test.equal(callCounts.registerTask, 3, 'registerTask should be called once');
+    test.equal(callCounts.run, 2, 'run should be called twice');
+    test.equal(callCounts.renameTask, 3, 'renameTask should be called once');
+    test.equal(callCounts.loadNpmTasks, 1, 'loadNpmTasks should be called once');
     test.done();
   }
 };
