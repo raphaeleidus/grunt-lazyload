e9214db11e8857bedc2902e97bf79b0366b2fb68
RaphaelEidus <raphaeleidus@gmail.com>
2014-02-04 12:23:49 -0500

    add test coverage for multi-target tasks

--- test/grunt-lazyload_test.js
+++ test/grunt-lazyload_test.js
@@ -24,18 +24,22 @@ var grunt_lazyloader = require('../lib/grunt-lazyload.js').lazyloader;
 
 var grunt, gruntInjecter = function() {
   //testing non-functional implementation of grunt
-  var callCounts = {registerTask: 0, run: 0, renameTask: 0, loadNpmTasks: 0};
-  var tasks = [];
-  return {
+  var callCounts = {registerTask: 0, run: 0, renameTask: 0, loadNpmTasks: 0}
+    , tasks = []
+    , mocks = {}
+    , module;
+  module = {
     task: {
-      registerTask: function(taskName, description, func) {
+      registerTask: function(taskName, description, fn) {
         callCounts.registerTask++;
-        tasks.push({name:taskName, task: func});
+        tasks.push({name:taskName, description: description, task: fn});
       },
       run: function(taskName) {
+        var taskParams = taskName.split(':');
+        taskName = taskParams.shift();
         callCounts.run++;
         var toRun = tasks.filter(function(t) { return t.name === taskName; });
-        toRun.forEach(function(t) { t.task(); });
+        toRun.forEach(function(t) { t.task.apply(t, taskParams); });
       },
       renameTask: function(orinalName, newName) {
         callCounts.renameTask++;
@@ -46,11 +50,18 @@ var grunt, gruntInjecter = function() {
         });
       }
     },
-    loadNpmTasks: function() {
+    loadNpmTasks: function(packageName) {
+      if(mocks[packageName]) {
+        module.task.registerTask(mocks[packageName].name, 'mocked '+mocks[packageName].name, mocks[packageName].fn);
+      }
       callCounts.loadNpmTasks++;
     },
-    getCurretCallCounts: function() { return callCounts; }
-  };
+    getCurretCallCounts: function() { return callCounts; },
+    mockNpmTask: function(packageName, taskName, fn) {
+      mocks[packageName] = { name: taskName, fn: fn };
+    }
+  }
+  return module;
 };
 
 exports['lazyloader'] = {
@@ -78,6 +89,24 @@ exports['lazyloader'] = {
       test.equal(callCounts.run, 2, 'run should be called twice');
       test.equal(callCounts.renameTask, 1, 'renameTask should be called once');
       test.equal(callCounts.loadNpmTasks, 1, 'loadNpmTasks should be called once');
+      test.done();
+    }
+  },
+  'single task with params': {
+    'register and run': function(test) {
+      test.expect(3);
+      //tests here
+      grunt_lazyloader.load('MultiTask', 'multiTask');
+      grunt_lazyloader.load('MultiTask-np', 'multiTask-np');
+      grunt.mockNpmTask('MultiTask', 'multiTask', function(param1, param2) {
+        test.equal(param1, 'foo', 'first paramater should be foo');
+        test.equal(param2, 'bar', 'second paramater should be bar');
+      });
+      grunt.mockNpmTask('MultiTask-np', 'multiTask-np', function() {
+        test.equal(arguments.length, 0, 'no paramaters passed');
+      });
+      grunt.task.run('multiTask:foo:bar');
+      grunt.task.run('multiTask-np');
       test.done();
     }
   },
